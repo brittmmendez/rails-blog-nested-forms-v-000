b7393856232fd4b2c568d76f64af9589341883a8
Brittany Morris <brittmmendez@gmail.com>
2018-02-20 21:24:53 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags
 
   validates_presence_of :name, :content
+
+  def tagss_attributes=(tag_attributes)
+  tag_attributes.values.each do |tag_attribute|
+    tag = Tag.find_or_create_by(tag_attribute)
+    self.tags << tag
+  end
 end
